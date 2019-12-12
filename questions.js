0ab9db237fface4ad16e4c3a583dc657b11730bb
johnathan olivas <jj.galvan13@yahoo.com>
2019-12-11 18:39:24 -0700

    added style.css and created functions in js

--- questions.js
+++ questions.js
@@ -16,3 +16,34 @@ Quiz.prototype.guess = function (answer) {
 
     this.questionIndex++;
 }
+
+Quiz.prototype.isEnded = function () {
+    return this.questionIndex === this.questions.length;
+}
+
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+Question.prototype.isCorrectAnswer = function (choice) {
+    return this.answer === choice;
+}
+function populate() {
+    if (quiz.isEnded()) {
+        showScores();
+    }
+    else {
+
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+
+        var choices = quiz.getQuestionIndex().choices;
+        for (var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+    }
+};
