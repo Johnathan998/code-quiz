20d746f2b9acbb03ad4e7e4de0f40ab0e8d59dcf
johnathan olivas <jj.galvan13@yahoo.com>
2019-12-11 17:35:21 -0700

    added div called app container

---
+++ questions.js
@@ -0,0 +1,18 @@
+var startButton = document.querySelector("#start");
+
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+Quiz.prototype.getQuestionIndex = function () {
+    return this.questions[this.questionIndex];
+}
+
+Quiz.prototype.guess = function (answer) {
+    if (this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+
+    this.questionIndex++;
+}
